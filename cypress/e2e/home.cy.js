93e0cb3fdc29fcafc18a4534887825530abaa584
Raj Khatri <ryukhatri@gmail.com>
2023-01-13 20:47:40 -0500

    home test case added

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,5 +1,6 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+const homepage = 'https://www.demoblaze.com/index.html'		
+describe('homepage test cases ', () => {
+  it('TC-HOME-001:Verify that user can browse the hompage and see 9 items as the product list', () => {
+    cy.visit(homepage)
   })
 })
