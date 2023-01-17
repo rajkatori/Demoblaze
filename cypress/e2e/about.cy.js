cd4f97861829294b19cdeb4d143c8346ce74c507
Raj Khatri <ryukhatri@gmail.com>
2023-01-16 21:13:59 -0500

    about added

---
+++ cypress/e2e/about.cy.js
@@ -0,0 +1,11 @@
+const homepage = 'https://www.demoblaze.com/index.html'
+describe('testing demoblaze about us',()=>{
+beforeEach(()=>{
+    cy.visit(homepage)
+})
+it('TC-ABOUT-001: Verify that video option is displayed when user click about us icon',()=>{
+    cy.get(':nth-child(3) > .nav-link').click().wait(2000)
+    cy.get('.vjs-poster').click()
+})
+
+})
